9c12bb416af30258bf866231a3841d6906fac593
HenriqueVAraujo <henriquevasconcelosaraujo@gmail.com>
2022-05-25 00:31:13 -0300

    Primeiro teste de emprego feito com sucesso, recebi muitos elogiosgit add *

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'red-main': '#c90914',
+        'blue-main': '#1793c9',
+      },
+      fontSize: {
+        '8px': '8px',
+      },
+    },
+  },
+  plugins: [],
+}
